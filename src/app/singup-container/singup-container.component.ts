6b4f0bf69fbbc580ae0bea4d7059b1ae7b5b6e28
Rodrigo Escobar Lopez <ocralo@gmail.com>
2023-09-24 21:31:11 +0000

    page-login

---
+++ src/app/singup-container/singup-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-singup-container',
+  templateUrl: './singup-container.component.html',
+  styleUrls: ['./singup-container.component.css']
+})
+export class SingupContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
